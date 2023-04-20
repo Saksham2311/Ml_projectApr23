ee408b2a799f77435cd58c8c4ea81ab6ddd9bf4f
Saksham Gupta <guptasaksham2311@gmail.com>
2023-04-20 06:54:34 +0530

    setup.py & req.txt

---
+++ setup.py
@@ -0,0 +1,27 @@
+#through this setup.py file we can create our whole machine learning application as a package
+#can deploy it on pypi
+# and can install it anywhere
+HYPEN_E_DOT='-e .'
+from setuptools import find_packages,setup
+from typing import List
+def get_requirements(file_path:str)->List[str]:
+    #this func will return the list of requirements
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("/n","") for req in requirements]
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+setup(
+    name='Ml_projectApr23',
+    version='0.0.1',
+    author='Saksham Gupta',
+    author_email='guptasaksham2311@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+    
+    
+)
---
+++ src/__init__.py
@@ -0,0 +1,1 @@
+#this src folder will be found as a package due to this __int__.py file
